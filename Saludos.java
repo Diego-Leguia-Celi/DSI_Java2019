2b8c3789acf00043f797ee67b896a437d4c18f99
Diego-Leguia-Celi <1124743@senati.pe>
2019-05-14 11:49:35 -0500

    subiendo

---
+++ Saludos.java
@@ -0,0 +1,7 @@
+public class Hola{
+	public static void main(String[] args){
+	  System.out.println("¡BIENVENIDOS!! \n Prometo esforzarme dia a dia , mucho mas que el modulo \n anterior.");
+	}
+
+
+}
